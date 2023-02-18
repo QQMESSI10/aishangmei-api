edcbf397cc512c2d762910789b31ea3c4a14f3f3
qinxiang <15163281896@163.com>
2023-02-18 13:13:06 +0800

    完善充卡历史查询

--- router_handler/card.js
+++ router_handler/card.js
@@ -5,11 +5,11 @@ const Card = require("../db/model/card");
 const UserCard = require("../db/model/userCard");
 const Recharge = require("../db/model/recharge");
 const RechargeProject = require("../db/model/rechargeProject");
-const UserCardRecord = require('../db/model/userCardRecord')
-const Server = require("../db/model/server")
+const UserCardRecord = require("../db/model/userCardRecord");
+const Server = require("../db/model/server");
 const User = require("../db/model/user");
 
-require('../db/relevancy')
+require("../db/relevancy");
 
 exports.list = (req, res) => {
   Card.findAndCountAll({
@@ -42,7 +42,13 @@ exports.recharge = (req, res) => {
       } else {
         userCardInfo = finducRes;
       }
-      Recharge.create({ userCardId: userCardInfo.id, money, serverId, remark, date })
+      Recharge.create({
+        userCardId: userCardInfo.id,
+        money,
+        serverId,
+        remark,
+        date,
+      })
         .then((rechargeRes) => {
           const rechargeId = rechargeRes.id;
           const rechargeProject = projectData.map((m) => {
@@ -56,16 +62,19 @@ exports.recharge = (req, res) => {
               return false;
             }
           });
-          const balance = userCardInfo.balance + Number(money)
-          const userCard = UserCard.update({ balance }, { where: { id: userCardInfo.id } })
+          const balance = userCardInfo.balance + Number(money);
+          const userCard = UserCard.update(
+            { balance },
+            { where: { id: userCardInfo.id } }
+          );
           const userCardRecord = UserCardRecord.create({
             userCardId: userCardInfo.id,
             type: 1,
             rechargeId,
             money,
             balance,
-            date
-          })
+            date,
+          });
           Promise.all(rechargeProject, userCard, userCardRecord)
             .then((upResArr) => {
               res.okput(rechargeId);
@@ -77,28 +86,116 @@ exports.recharge = (req, res) => {
     .catch((findErr) => seqError(findErr, res));
 };
 
-exports.rechargeList = (req, res) => {
-  Recharge.findAndCountAll({
-    where: {},
-    offset: req.body.offset,
-    limit: req.body.limit,
-    order: [[req.body.orderBy || "createdAt", "DESC"]],
-    include: [{
-      model: UserCard,
-      include: [User, Card]
-    }, Server
-    ]
-  }).then(findRes => {
-    const list = findRes.rows.map(m => {
-      const { id, date, money } = m
-      return {
-        id, date, money,
-        user: m.userCard.user.name,
-        telephone: m.userCard.user.telephone,
-        card: m.userCard.card.name,
-        server: m.server.name
-      }
+exports.rechargeList = async (req, res) => {
+  let userCardList = null;
+  let serverList = null;
+  let isEmpty = false;
+  if (req.body.name || req.body.telephone) {
+    await User.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${req.body.name || ""}%`,
+        },
+        telephone: {
+          [Op.like]: `%${req.body.telephone || ""}%`,
+        },
+      },
     })
-    res.okput({ total: findRes.count, list });
-  }).catch((findErr) => seqError(findErr, res));
-}
+      .then(async (userRes) => {
+        if (userRes.length > 0) {
+          const userList = userRes.map((m) => {
+            return m.id;
+          });
+          await UserCard.findAll({
+            where: {
+              userId: {
+                [Op.in]: userList,
+              },
+            },
+          })
+            .then((ucRes) => {
+              if (ucRes.length > 0) {
+                userCardList = ucRes.map((m) => {
+                  return m.id;
+                });
+              } else {
+                isEmpty = true;
+              }
+            })
+            .catch((ucErr) => seqError(ucErr, res));
+        } else {
+          isEmpty = true;
+        }
+      })
+      .catch((userErr) => seqError(userErr, res));
+  }
+  if (req.body.server) {
+    await Server.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${req.body.server || ""}%`,
+        },
+      },
+    }).then((serRes) => {
+      if (serRes.length > 0) {
+        serverList = serRes.map((m) => {
+          return m.id;
+        });
+      } else {
+        isEmpty = true;
+      }
+    });
+  }
+  if (!isEmpty) {
+    let where = {};
+    if (req.body.date) {
+      // moment(this.getDataValue("date")).format("YYYY-MM-DD HH:mm:ss");
+      const beginDate = new Date(req.body.date + ' 00:00:00')
+      const endDate = new Date(req.body.date + " 23:59:59");
+      where.date = {
+        [Op.between]: [beginDate, endDate],
+      };
+    }
+    if (userCardList) {
+      where.userCardId = {
+        [Op.in]: userCardList,
+      };
+    }
+    if (serverList) {
+      where.serverId = {
+        [Op.in]: serverList,
+      };
+    }
+    Recharge.findAndCountAll({
+      where,
+      offset: req.body.offset,
+      limit: req.body.limit,
+      order: [[req.body.orderBy || "createdAt", "DESC"]],
+      include: [
+        {
+          model: UserCard,
+          include: [User, Card],
+        },
+        Server,
+      ],
+    })
+      .then((findRes) => {
+        const list = findRes.rows.map((m) => {
+          const { id, date, money } = m;
+          return {
+            id,
+            date,
+            money,
+            user: m.userCard.user.name,
+            telephone: m.userCard.user.telephone,
+            card: m.userCard.card.name,
+            server: m.server.name,
+          };
+        });
+        res.okput({ total: findRes.count, list });
+      })
+      .catch((findErr) => seqError(findErr, res));
+  } else {
+    res.okput({ total: 0, list: [] });
+  }
+};
